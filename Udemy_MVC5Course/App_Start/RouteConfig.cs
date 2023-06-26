0be22a2f16d966533ac39bb5035fa1402a353338
TusharKhaire <tusharkhaire099@gmail.com>
2023-06-26 19:48:00 +0530

    A

--- Udemy_MVC5Course/App_Start/RouteConfig.cs
+++ Udemy_MVC5Course/App_Start/RouteConfig.cs
@@ -12,9 +12,7 @@ namespace Udemy_MVC5Course
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-
-
+                       
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
